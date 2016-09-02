60ca0154c3f6c1b0e21b6ab403ae03d6bdf81fd6
tommy3878 <qiantommy@gmail.com>
2016-09-02 14:52:48 +0800

    Update task5.m

--- task5.m
+++ task5.m
@@ -10,34 +10,41 @@ function [R,G,B] = WaveLengthToRGB( WaveLength )
     R=255(440-w)/60;
     G=2550;
     B=2551;
-    %Blue - Cyan
+  
+  %Blue - Cyan
   elseif 440<w<=490
     R=2550;
     G=255(w-440)/50;
     B=2551;
-    %Cyan- Green
+  
+  %Cyan- Green
   elseif 490<w<=510
     R=2550;
     G=2551;
     B=255(510-w)/20;
-    %Green - Yellow
+  
+  %Green - Yellow
   elseif 510<w<=580
     R=255(w-510)/70;
     G=2551;
     B=2550;
-    %Green - Yellow
+  
+  %Green - Yellow
   elseif 580<w<=645
     R=2551;
     G=255(645-w)/65;
     B=2550;
-    %Yellow - Red
+  
+  %Yellow - Red
   elseif 580<w<=645
     R=2551;
     G=2550;
     B=2550;
-    %Not in the specific range
+  
+  %Not in the specific range
   else 
     R=0;
     G=0;
     B=0;
+
 end
