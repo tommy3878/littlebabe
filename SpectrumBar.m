10c797f2abb588d0047ea8d62da49246e36d57e7
tommy3878 <qiantommy@gmail.com>
2016-09-03 10:56:44 +0800

    Add files via upload

---
+++ SpectrumBar.m
@@ -0,0 +1,11 @@
+function [ S ] = SpectrumBar( Lw,Hw )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+for i=1:100
+    for j=1:n
+     n = Hw- Lw + 1
+    end
+        for z=1:3
+display(S)
+end
+
