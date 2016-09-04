9565fdafb1b80332f853fdf72f076d25ca855462
Thomas Qian <thomasq@yahoo-inc.com>
2016-09-04 10:58:31 +0800

    update

--- SuppliedCode/ExtractWaveLengthFromFilename.m
+++ SuppliedCode/ExtractWaveLengthFromFilename.m
@@ -1,8 +1,19 @@
 function [ value ] = ExtractWaveLengthFromFilename( str )
-
-    % http://www.mathworks.com/help/matlab/matlab_prog/regular-expressions.html
-    value = regexp(str, '_f.*n_|_f.*m_|_f.*w_', 'match');
     
-    value = regexp(value, '[0-9]{3}', 'match');
+    try
+        % http://www.mathworks.com/help/matlab/matlab_prog/regular-expressions.html
+        value = regexp(str, '_f.*n_|_f.*m_|_f.*w_', 'match');
+
+        % Extract the numeric value
+        value = regexp(value, '[0-9]*', 'match');
+
+        % Convert to double for further processing
+        value = str2double(value{1});
+    
+    catch
+        % On error that cant find the value, return -1
+        value = -1;
+        
+    end
     
 end
