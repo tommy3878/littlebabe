252ebb9206da64547cc4b1f97fd8b4b5615242c7
Thomas Qian <thomasq@yahoo-inc.com>
2016-09-04 11:00:41 +0800

    update

--- SuppliedCode/ExtractWaveLengthFromFilename.m
+++ SuppliedCode/ExtractWaveLengthFromFilename.m
@@ -8,10 +8,15 @@ function [ value ] = ExtractWaveLengthFromFilename( str )
         value = regexp(value, '[0-9]*', 'match');
 
         % Convert to double for further processing
+        % For some reason it only return 1x1 cell value within the
+        % function, but not the case when execute directly within command
+        % window .... So further process to distill and convert the value
+        % to double
         value = str2double(value{1});
     
     catch
         % On error that cant find the value, return -1
+        % http://www.mathworks.com/help/matlab/matlab_prog/use-trycatch-to-handle-errors.html
         value = -1;
         
     end
